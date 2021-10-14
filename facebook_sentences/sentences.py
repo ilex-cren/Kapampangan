cc7dc4f448323615dede14298782624de32add67
mochinokiladra <holly.redman@gmail.com>
2021-10-14 17:10:46 -0400

    added facebook sentences

---
+++ facebook_sentences/sentences.py
@@ -0,0 +1,10 @@
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+def main():
+   f = open('facebook_sentences.txt', 'r')
+   raw = f.read().strip()
+   sentences = sent_tokenize(raw)
+   print(sentences)
+   print(len(sentences))
+main()
