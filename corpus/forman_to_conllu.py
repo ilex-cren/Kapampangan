e080750f7499c65f4bed03ee77afcfa13cd88ea5
mochinokiladra <holly.redman@gmail.com>
2021-11-28 11:29:58 -0500

    fixed forman_to_conllu.py and updated forman.conllu

--- corpus/forman_to_conllu.py
+++ corpus/forman_to_conllu.py
@@ -1,4 +1,5 @@
 import re
+from nltk.tokenize import word_tokenize, sent_tokenize
 f = open("forman.txt", 'r')
 items = re.split(r'\s*\n\s*\n\s*',f.read()) 
 for item in items:
@@ -14,7 +15,13 @@ for item in items:
       eng = eng.replace(i, j)
    print('# text[eng] =', eng)
    print('# labels = forman:71:', page.group(), sep='')
+   for i in range(len(word_tokenize(pam))):
+     number = str(i)
+     word = word_tokenize(pam)[i]
+     line = number + '\t' + word + 8 * ('\t' + '_')
+     print(line)
    print('\n')
+
 f.close()
 
 
