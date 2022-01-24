70d3bf62fbe5ff62d73d4cad1b91f81a04f30ec2
mochinokiladra <holly.redman@gmail.com>
2022-01-24 12:35:59 -0500

    edited add_metadata.py

--- corpus/facebook/add_metadata.py
+++ corpus/facebook/add_metadata.py
@@ -1,23 +1,13 @@
-# add metadata to facebook_sentences.txt.conllu
-import csv
+# add metadata to facebook_sentences.conllu
 
-def get_text(sent):
-   sentence = ''
-   columns = csv.reader(sent, delimiter='\t')
-   for word in columns[1]:
-      sentence += ' '
-      sentence += word
-      
-f1 = open('facebook_sentences.txt.conllu', 'r')
-f2 = open('facebook.conllu', 'w')
-
+f = open('facebook_sentences.conllu', 'r')
 metadata = '\n# sent_id =\n# text ={}\n# text[eng] =\n# labels ='
-lines = f1.read().split('\n\n')
 
-for line in lines:
-   print(metadata.format(get_text(line)))
-   print(line)
+# read lines from facebook_sentences.conllu
 
-f1.close()
-f2.close()
+# if line begins with 1, insert metadata above it
+#   - text = words from column 2 in sentence form
+#   - text[eng] leave blank for now
+#   - labels blank for now?
 
+f.close()
