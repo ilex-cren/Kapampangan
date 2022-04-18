2c60739fd4a45ae32be0a845db305b6b3d520ec5
mochinokiladra <holly.redman@gmail.com>
2022-04-18 11:55:51 -0400

    added some sentences, edited python script

--- corpus/facebook/facebook-to-conllu.py
+++ corpus/facebook/facebook-to-conllu.py
@@ -4,7 +4,8 @@ import re
 # first read in all the input and replace the delimiter
 # with a standard one
 inp = sys.stdin.read()
-inp = re.sub('^[—-]+$', '----------', inp)
+inp = re.sub('\n[—-]+\n?', '\n----------\n', inp)
+
 
 block_id = 1
 sent_id = 1
@@ -26,30 +27,30 @@ for block in inp.split('----------'):
 
 	# define a new function called "segment" that takes a single argument, line
 	def segment(line):
-		outp = line
+                outp = line
 		# replace any number of sequential ! ? or . with that 
 		# and a newline
-		outp = re.sub('([!.?]+)', '\g<1>\n', outp)
-		outp = re.sub('([!.?]+)\n"', '\g<1>"\n', outp)
-		outp = re.sub('(["]+)',' " ', outp)
-		outp = re.sub('([ ]+)', ' ', outp)
+                outp = re.sub('([!.?]+)', '\g<1>\n', outp)
+                outp = re.sub('([!.?]+)\n"', '\g<1>"\n', outp)
+                outp = re.sub('(["]+)',' " ', outp)
+                outp = re.sub('([ ]+)', ' ', outp)
 		# ['káyi Kapampángán nómang Gen Z déng tútúlak kang Bongbong kéti...', ' dána...', ' bísá lá mú yátang live Sábung ding alti...', '']
-		outp = re.sub('([!.?]+)\n( *dána)([!.?]+)\n', '\g<1>\g<2>\g<3>', outp)
+                outp = re.sub('([!.?]+)\n( *dána)([!.?]+)\n', '\g<1>\g<2>\g<3>', outp)
 		# split the string on newlines to get the list of sentences
-		return outp.split('\n')
+                return outp.split('\n')
 
 	# a function to tokenise sentences into tokens
 	# for example "This is a test." -> ["This", "is", "a", "test", "."]
 	def tokenise(sent):
-		outp = sent
+                outp = sent
 		# add spaces around punctuation characters
-		outp = re.sub('([!.?]+)', ' \g<1> ', outp)
-		outp = re.sub('(,)', ' \g<1> ', outp)
+                outp = re.sub('([!.?:]+)', ' \g<1> ', outp)
+                outp = re.sub('(,)', ' \g<1> ', outp)
 		# replace multiple spaces
-		outp = re.sub('  *', ' ', outp)
+                outp = re.sub('  *', ' ', outp)
 		# remove spaces at the beginning and end
-		outp = outp.strip()
-		return outp.split(' ')
+                outp = outp.strip()
+                return outp.split(' ')
 
 	# next, for each of the sentences in the line. for this we need
 	# a tokenisation function.
